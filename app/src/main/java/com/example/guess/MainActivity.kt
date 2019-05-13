b1b877c6835ffb055645711a313938b8c7c6e138
Nightt1106 <40443125@gm.nfu.edu.tw>
2019-05-13 23:27:37 +0800

    Function Design , Toast , A;ertDialog

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,12 +16,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Mainactivity","SecertNumber : ${secretNumber.secret}")
     }
 
     fun check(view:View){
         val n:Int = number.text.toString().toInt()
         println("number: $n")
-        Log.d("MainActivity","number $n")
+//        Log.d("MainActivity","number $n")
+        val diff:Int = secretNumber.validate(n)
+        var message = "You got it"
+        if(diff < 0){
+            message = "Bigger"
+        } else if(diff > 0){
+            message = "Smaller"
+        }
+//        Toast.makeText(this , message , Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton(R.string.ok,null)
+            .show()
     }
-
 }
--- app/src/main/java/com/example/guess/SecretNumber.kt
+++ app/src/main/java/com/example/guess/SecretNumber.kt
@@ -16,4 +16,5 @@ fun main(){
     val secretNumber = SecretNumber()
     println(secretNumber.secret)
     println("${secretNumber.validate(2)} , count: ${secretNumber.count}")
+
 }
