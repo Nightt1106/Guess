b226aff868c0072a6ebff8c47c8c97b8f723b691
Nightt1106 <40443125@gm.nfu.edu.tw>
2019-05-13 22:21:31 +0800

    layout

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val secretNumber = SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        R.drawable.dog
     }
+
+    fun check(view:View){
+        val n:Int = number.text.toString().toInt()
+        println("number: $n")
+        Log.d("MainActivity","number $n")
+    }
+
 }
